01f6c0ff4bed245a66198cdafff5ff5484517e92
ha-matsumu <ha.matsumu@gmail.com>
2018-12-20 00:07:30 +0900

    updated quiz.js

--- quiz.js
+++ quiz.js
@@ -1,1 +1,18 @@
-console.log("test");
+class Quiz {
+    constructor(_url) {
+      this.data = fetch(_url)
+        .then(response => {
+          return response.json();
+        })
+        .then(response => {
+          return response.results;
+        })
+        .then(data => {
+          return data;
+        });
+    }  
+  }
+  
+  const quiz = new Quiz("https://opentdb.com/api.php?amount=10");
+  console.log("クイズデータ : ", quiz.data);
+  
