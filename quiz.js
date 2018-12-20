adddc8d96403231926ecf407d37e750483efb7d7
ha-matsumu <ha.matsumu@gmail.com>
2018-12-20 22:47:27 +0900

    modified quiz.js

--- quiz.js
+++ quiz.js
@@ -1,18 +1,31 @@
+let quizDataList;
+let currentQuizIndex = 0;
+
 class Quiz {
-    constructor(_url) {
-      this.data = fetch(_url)
-        .then(response => {
-          return response.json();
-        })
-        .then(response => {
-          return response.results;
-        })
-        .then(data => {
-          return data;
-        });
+    constructor(_quizDataList, _currentQuizIndex) {
+        this.quiz = _quizDataList[_currentQuizIndex];
+        this.question = this.quiz.question;
+
+        this.answers = this.quiz.incorrect_answers.slice();
+        this.answers.push(this.quiz.correct_answer);
+        
+        this.correctAnswer = this.quiz.correct_answer;
     }  
-  }
-  
-  const quiz = new Quiz("https://opentdb.com/api.php?amount=10");
-  console.log("クイズデータ : ", quiz.data);
-  
+}
+
+fetch("https://opentdb.com/api.php?amount=10")
+    .then(response => {
+        return response.json();
+    })
+    .then(response => {
+        return response.results;
+    })
+    .then(data => {
+        quizDataList = data;
+
+        console.log("クイズデータ : ", quizDataList);
+        const quiz = new Quiz(quizDataList, currentQuizIndex);
+        console.log("問題文 : ", quiz.question);
+        console.log("解答一覧 : ", quiz.answers);
+        console.log("正解 : ", quiz.correctAnswer);
+    });  
