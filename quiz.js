5b5acba3cf16bb8e246735dc93c1e2c6405e9298
ha-matsumu <ha.matsumu@gmail.com>
2018-12-21 16:09:50 +0900

    modified quiz.js

--- quiz.js
+++ quiz.js
@@ -2,8 +2,10 @@ let quizDataList = [];
 let currentQuizIndex = 0;
 
 class Quiz {
-  constructor(_quizData) {
-    this.quiz = _quizData;
+  constructor(_quizInstance) {
+    this.question = _quizInstance.question;
+    this.correctAnswer = _quizInstance.correct_answer;
+    this.incorrectAnswers = _quizInstance.incorrect_answers;
   }
 
   // 以下に解答一覧をシャッフルする機能の実装
@@ -16,13 +18,15 @@ fetch("https://opentdb.com/api.php?amount=10")
   .then(response => {
     return response.results;
   })
-  .then(quizDatas => {
-    quizDatas.forEach(quizData => {
-      quizDataList.push(new Quiz(quizData));
+  .then(quizInstances => {
+    quizInstances.forEach(quizInstance => {
+      quizDataList.push(new Quiz(quizInstance));
     });
 
-    console.log("クイズデータ一覧 : ", quizDatas);
-    quizDataList.forEach((quizData, value) => {
-      console.log("クイズ", value + 1, " : ", quizData);
+    console.log("クイズデータ一覧 : ", quizInstances);
+    quizDataList.forEach((quizInstance, value) => {
+      console.log("クイズ", value + 1, "問題 : ", quizInstance.question);
+      console.log("クイズ", value + 1, "正解 : ", quizInstance.correctAnswer);
+      console.log("クイズ", value + 1, "不正解 : ", quizInstance.incorrectAnswers);
     });
   });
