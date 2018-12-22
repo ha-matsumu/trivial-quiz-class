eff63247ffe0970fc28819ed1846d9cbb9546f0f
ha-matsumu <ha.matsumu@gmail.com>
2018-12-22 21:17:50 +0900

    modified quiz.js

--- quiz.js
+++ quiz.js
@@ -1,11 +1,14 @@
-let quizDataList = [];
+const quizInstances = [];
 let currentQuizIndex = 0;
 
 class Quiz {
-  constructor(_quizInstance) {
-    this.question = _quizInstance.question;
-    this.correctAnswer = _quizInstance.correct_answer;
-    this.incorrectAnswers = _quizInstance.incorrect_answers;
+  constructor(_quizData) {
+    this.category = _quizData.category;
+    this.correctAnswer = _quizData.correct_answer;
+    this.difficulty = _quizData.difficulty;
+    this.incorrectAnswers = _quizData.incorrect_answers;
+    this.question = _quizData.question;
+    this.type = _quizData.type;
   }
 
   // 以下に解答一覧をシャッフルする機能の実装
@@ -18,15 +21,19 @@ fetch("https://opentdb.com/api.php?amount=10")
   .then(response => {
     return response.results;
   })
-  .then(quizInstances => {
-    quizInstances.forEach(quizInstance => {
-      quizDataList.push(new Quiz(quizInstance));
+  .then(quizDataList => {
+    quizDataList.forEach(quizData => {
+      quizInstances.push(new Quiz(quizData));
     });
 
     console.log("クイズデータ一覧 : ", quizInstances);
-    quizDataList.forEach((quizInstance, value) => {
-      console.log("クイズ", value + 1, "問題 : ", quizInstance.question);
-      console.log("クイズ", value + 1, "正解 : ", quizInstance.correctAnswer);
-      console.log("クイズ", value + 1, "不正解 : ", quizInstance.incorrectAnswers);
+    quizInstances.forEach((quizData, value) => {
+      console.log("----QUIZ.", value + 1, "----");
+      console.log("category", quizData.category);
+      console.log("correct_answer : ", quizData.correctAnswer);
+      console.log("difficulty: ", quizData.difficulty);
+      console.log("incorrect_answers : ", quizData.incorrectAnswers);
+      console.log("question : ", quizData.question);
+      console.log("type : ", quizData.type);
     });
   });
