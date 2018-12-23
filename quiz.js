c241b8674d91cdbddee97a90efa1791e5409e99d
ha-matsumu <ha.matsumu@gmail.com>
2018-12-23 22:35:20 +0900

    modified trivial-quiz-class

--- quiz.js
+++ quiz.js
@@ -1,12 +1,3 @@
-const divCurrentQuizQuestion = document.getElementById("currentQuizQuestion");
-const ulCurrentQuizAnswers = document.getElementById("currentQuizAnswers");
-const divNumberOfCorrectAnswers = document.getElementById(
-  "numberOfCorrectAnswers"
-);
-const quizInstances = [];
-let currentQuizIndex = 0;
-let numberOfCorrectAnswers = 0;
-
 class Quiz {
   constructor(_quizData) {
     this.category = _quizData.category;
@@ -31,64 +22,4 @@ class Quiz {
 
     return answers;
   }
-}
-
-fetch("https://opentdb.com/api.php?amount=10")
-  .then(response => {
-    return response.json();
-  })
-  .then(response => {
-    return response.results;
-  })
-  .then(quizDataList => {
-    quizDataList.forEach(quizData => {
-      quizInstances.push(new Quiz(quizData));
-    });
-
-    // TODO:あとで消す
-    console.log("クイズデータ一覧 : ", quizInstances);
-
-    appendCurrentQuizToContainer(quizInstances[currentQuizIndex]);
-  });
-
-function appendCurrentQuizToContainer(_currentQuizInstance) {
-  const currentQuestionText = `Q${currentQuizIndex + 1}. ${
-    _currentQuizInstance.question
-  }`;
-  const shuffledAnswers = _currentQuizInstance.getShuffledAnswers();
-
-  divCurrentQuizQuestion.textContent = currentQuestionText;
-
-  while (ulCurrentQuizAnswers.firstChild) {
-    ulCurrentQuizAnswers.removeChild(ulCurrentQuizAnswers.firstChild);
-  }
-
-  shuffledAnswers.forEach(shuffledAnswer => {
-    const liCurrentQuizAnswer = document.createElement("li");
-    liCurrentQuizAnswer.textContent = shuffledAnswer;
-
-    liCurrentQuizAnswer.addEventListener("click", () => {
-      currentQuizIndex++;
-      if (currentQuizIndex === quizInstances.length) {
-        const resultText = `Your Score<br>${numberOfCorrectAnswers} / ${
-          quizInstances.length
-        }`;
-        divNumberOfCorrectAnswers.innerHTML = resultText;
-      } else {
-        if (liCurrentQuizAnswer.textContent === _currentQuizInstance.correctAnswer) {
-          numberOfCorrectAnswers++;
-          alert("You got it right!!");
-        } else {
-          alert(
-            `You got it wrong. The answer of this question is "${
-                _currentQuizInstance.correctAnswer
-            }".`
-          );
-        }
-        appendCurrentQuizToContainer(quizInstances[currentQuizIndex]);
-      }
-    });
-
-    ulCurrentQuizAnswers.appendChild(liCurrentQuizAnswer);
-  });
 }
